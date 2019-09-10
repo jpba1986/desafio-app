0ae792eb866cd381979e78662b49a59edca26a52
unknown <juanpablo.aguila@gmail.com>
2019-09-09 21:28:40 -0300

    forecast components and basic routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { Routes, RouterModule} from '@angular/router';
 
+import { AppComponent } from './app.component';
+import { ForecastComponent } from './pages/forecast/forecast.component';
+import { ForecastListComponent } from './pages/forecast/forecast-list/forecast-list.component';
+import { ForecastDetailComponent } from './pages/forecast/forecast-detail/forecast-detail.component';
+
+const appRoutes: Routes=[
+  { path: '', redirectTo: '/', pathMatch:'full'},
+  { path: 'forecast', component: ForecastComponent,
+    children:[
+      {path: 'id', component:ForecastDetailComponent}
+    ]
+  }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ForecastComponent,
+    ForecastListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
